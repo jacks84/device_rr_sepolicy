77c3aea12a152e2718353ac0212f21d3b3803a71
mracar07 <umutcan@umutcanacar.me>
2019-11-19 19:35:00 +0000

    RR-ifyy
    Change-Id: I112bb9055a4bb44ee37c605d863da9bce62f06bd
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -10,14 +10,14 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/rr/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/system
+    device/rr/sepolicy/qcom/dynamic \
+    device/rr/sepolicy/qcom/system
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/vendor
+    device/rr/sepolicy/qcom/dynamic \
+    device/rr/sepolicy/qcom/vendor
 endif
